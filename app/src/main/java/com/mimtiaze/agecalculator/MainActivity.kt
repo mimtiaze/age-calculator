ee4bc9b9023e041e59d802a36e98683da06ab2ee
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-05-21 17:00:03 +0600

    design done

--- app/src/main/java/com/mimtiaze/agecalculator/MainActivity.kt
+++ app/src/main/java/com/mimtiaze/agecalculator/MainActivity.kt
@@ -6,14 +6,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.Toast
 import java.util.Calendar
+import java.util.Date
 
 class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
-    private val myCalender: Calendar = Calendar.getInstance()
-    private val year = myCalender.get(Calendar.YEAR)
-    private val month = myCalender.get(Calendar.MONTH)
-    private val day = myCalender.get(Calendar.DAY_OF_MONTH)
+    private val todayCal: Calendar = Calendar.getInstance()
+    private val year = todayCal.get(Calendar.YEAR)
+    private val month = todayCal.get(Calendar.MONTH)
+    private val day = todayCal.get(Calendar.DAY_OF_MONTH)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +35,13 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         dpd.show()
     }
 
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+    override fun onDateSet(view: DatePicker?, selectedYear: Int, selectedMonth: Int, selectedDayOfMonth: Int) {
         //Toast.makeText(this, "${month+1}/$dayOfMonth/$year", Toast.LENGTH_SHORT).show()
+
+        var passedYear =  year - selectedYear
+        var passedMonth = month - selectedMonth
+        var passedDay = day - selectedDayOfMonth
+
+        Toast.makeText(this, "Your are $passedYear years, $passedMonth months, $passedDay days old", Toast.LENGTH_SHORT).show()
     }
 }
